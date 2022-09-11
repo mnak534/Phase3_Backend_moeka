38fc999dddf03e3cce0864645c3d9d2d6a225370
Moeka <monjiro1025@gmail.com>
2022-09-12 03:02:38 +1200

    Following 2020 tutorial

--- Phase3MSA_backend_moeka4/Models/Pizza.cs
+++ Phase3MSA_backend_moeka4/Models/Pizza.cs
@@ -1,7 +1,12 @@
 ﻿namespace Phase3MSA_backend_moeka4.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public class Pizza
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsVegan { get; set; }
