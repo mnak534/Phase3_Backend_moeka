b18443a366bd8d53b987aeb5c770adf4f1edd120
Moeka <monjiro1025@gmail.com>
2022-09-13 19:36:26 +1200

    Created unit tests for PizzaController

--- Phase3MSA_backend_moeka4/Program.cs
+++ Phase3MSA_backend_moeka4/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PizzaApi.Infrastructure.Contexts;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,15 @@ builder.Services.AddDbContext<ApiContext>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Pizza API",
+        Description = "Make a pizza menu",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
---
+++ PizzaApi.UnitTests/PizzaControllerTest.cs
@@ -0,0 +1,169 @@
+using NUnit.Framework;
+using PizzaApi.Domain.Models;
+using PizzaApi.Infrastructure.Contexts;
+using PizzaApi.Controllers;
+using NSubstitute;
+using Microsoft.EntityFrameworkCore;
+using FluentAssertions;
+
+namespace PizzaApi.UnitTests;
+
+public class PizzagControllerTest
+{
+    private List<Pizza> pizzas;
+
+    private PizzaController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        pizzas = new List<Pizza>
+        {
+            new Pizza
+            {
+                Id = 0,
+                Name = "mushroom1",
+                IsVegan = false
+
+            },
+            new Pizza
+            {
+                Id = 0,
+                Name = "mushroom2",
+                IsVegan = false
+            },
+            new Pizza
+            {
+                Id = 0,
+                Name = "mushroom3",
+                IsVegan = false
+            }
+        };
+    }
+
+    [Test]
+    public void TestGet()
+    {
+        var options = new DbContextOptionsBuilder<ApiContext>().UseInMemoryDatabase("TestGetPizza").Options;
+
+        var context = new ApiContext(options);
+
+        context.PizzaMenu.AddRange(pizzas);
+        context.SaveChanges();
+
+        _controller = Substitute.For<PizzaController>(context);
+
+        var result = _controller.Get(1);
+        result.Result.Value.Id.Should().Be(1);
+        result.Result.Value.Name.Should().Be("mushroom1");
+        result.Result.Value.IsVegan.Should().Be(false);
+    }
+
+    [Test]
+    public void TestPut()
+    {
+        var options = new DbContextOptionsBuilder<ApiContext>().UseInMemoryDatabase("TestPut").Options;
+        var context = new ApiContext(options);
+
+        context.PizzaMenu.AddRange(pizzas);
+        context.SaveChanges();
+        _controller = Substitute.For<PizzaController>(context);
+
+        Pizza pizzaToPut = new Pizza
+        {
+            Id = 0,
+            Name = "mushroom4",
+            IsVegan = false
+        };
+
+        _controller.Create(pizzaToPut);
+
+        var result = _controller.Get(4);
+
+        Pizza resultPizza = result.Result.Value;
+        resultPizza.Should().BeEquivalentTo(pizzaToPut);
+    }
+
+    [Test]
+    public void TestPost()
+    {
+        var options = new DbContextOptionsBuilder<ApiContext>().UseInMemoryDatabase("TestPostHotelBooking").Options;
+
+        var context = new ApiContext(options);
+
+        context.PizzaMenu.AddRange(pizzas);
+        context.SaveChanges();
+
+        _controller = Substitute.For<PizzaController>(context);
+
+        Pizza pizzaToPost = new Pizza
+        {
+            Id = 4,
+            Name = "Super Mushroom",
+            IsVegan = true
+        };
+
+        _controller.Edit(pizzaToPost);
+
+        var result = _controller.Get(1);
+        result.Result.Value.Id.Should().Be(1);
+        result.Result.Value.Name.Should().Be("Super Mushroom");
+        result.Result.Value.IsVegan.Should().Be(true);
+
+    }
+
+    [Test]
+    public void TestDelete()
+    {
+        var options = new DbContextOptionsBuilder<ApiContext>().UseInMemoryDatabase("TestDelete").Options;
+        var context = new ApiContext(options);
+
+        context.PizzaMenu.AddRange(pizzas);
+        context.SaveChanges();
+
+        _controller = Substitute.For<PizzaController>(context);
+
+        _controller.Delete(4);
+
+        List<Pizza> shouldBe = new List<Pizza>
+        {
+            new Pizza
+            {
+                Id = 1,
+                Name = "mushroom",
+                IsVegan = false
+
+            },
+            new Pizza
+            {
+                Id = 2,
+                Name = "mushroom2",
+                IsVegan = false
+            },
+            new Pizza
+            {
+                Id = 3,
+                Name = "mushroom3",
+                IsVegan = false
+            }
+        };
+
+        List<Pizza> newList = (List<Pizza>)_controller.GetAll().Result.Value;
+        newList.Should().BeEquivalentTo(shouldBe);
+    }
+
+    [Test]
+    public void TestGetAll()
+    {
+        var options = new DbContextOptionsBuilder<ApiContext>().UseInMemoryDatabase("TestGetAllHotelBookings").Options;
+        var context = new ApiContext(options);
+
+        context.PizzaMenu.AddRange(pizzas);
+        context.SaveChanges();
+
+        _controller = Substitute.For<PizzaController>(context);
+
+        List<Pizza> allHotelBookings = (List<Pizza>)_controller.GetAll().Result.Value;
+        allHotelBookings.Should().BeEquivalentTo(pizzas);
+    }
+}
